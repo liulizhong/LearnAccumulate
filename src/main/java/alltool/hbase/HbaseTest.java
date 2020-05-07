858c1f71f93ba159141ea86ec2c665516ebabcca
liulizhong <liu31646608@126.com>
2020-05-07 18:20:55 +0800

    my.cnf

---
+++ src/main/java/alltool/hbase/HbaseTest.java
@@ -0,0 +1,19 @@
+package alltool.hbase;
+
+import java.io.IOException;
+
+/**
+ * @author lizhong.liu
+ * @version TODO
+ * @class ??
+ * @CalssName HbaseTest
+ * @create 2020-05-07 17:52
+ * @Des TODO
+ */
+public class HbaseTest {
+    public static void main(String[] args) throws IOException {
+        System.out.println(HbaseAPI.isTableExist("testNamel"));
+
+
+    }
+}
