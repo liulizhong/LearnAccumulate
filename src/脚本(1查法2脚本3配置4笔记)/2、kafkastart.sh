16915a505116d4716b72cd4907995c105bc524b2
liulizhong <liu31646608@126.com>
2020-06-09 18:15:23 +0800

    add all2

--- src/脚本(1查法2脚本3配置4笔记)/2、kafkastart.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-for i in atguigu@hadoop102 atguigu@hadoop103 atguigu@hadoop104
-do
-        echo "================           $i             ================"
-        /opt/module/kafka_2.11-0.11.0.2/bin/kafka-server-start.sh /opt/module/kafka_2.11-0.11.0.2/config/server.properties &
-        /opt/module/kafka_2.11-0.11.0.2/bin/kafka-server-start.sh /opt/module/kafka_2.11-0.11.0.2/config/server.properties'
-##      ssh $i '/opt/module/kafka_2.11-0.11.0.2/bin/kafka-server-stop.sh /opt/module/kafka_2.11-0.11.0.2/config/server.properties'   ## kafkastop.sh
-done
