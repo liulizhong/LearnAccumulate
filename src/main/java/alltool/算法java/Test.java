081441bf2db964beebb7d45642d2835cba1a2141
liulizhong <liu31646608@126.com>
2021-03-07 18:47:40 +0800

    my.cnf

---
+++ src/main/java/alltool/算法java/Test.java
@@ -0,0 +1,19 @@
+package alltool.算法java;
+
+import java.util.Arrays;
+
+/**
+ * @author lizhong.liu
+ * @version TODO
+ * @class ??
+ * @CalssName Test
+ * @create 2021-01-15 13:53
+ * @Des TODO
+ */
+public class Test {
+    public static void main(String[] args) {
+        int[] arr = {5, 3, 0, 9, 1, 2, 3, 4};
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
--- src/main/test/liulizhong/Test.java
+++ src/main/test/liulizhong/Test.java
@@ -1,35 +1,7 @@
 package liulizhong;
 
-import alltool.hbase.HbaseAPI;
-
-import java.io.IOException;
-
-/**
- * @author lizhong.liu
- * @version TODO
- * @class ??
- * @CalssName Test
- * @create 2020-07-30 10:13
- * @Des TODO
- */
 public class Test {
     public static void main(String[] args) throws IOException {
-        int i = 0;
-        int j = 0;
-        while (i < 10) {
-            while (j <= 5) {
-                if (i == 8) {
-                    return;
-                }
-                if (j == 2) {
-                    break;
-                }
-                j++;
-            }
-            i++;
-        }
-        System.out.println("外边代码执行了");
-        System.out.println(i);
-        System.out.println(j);
+        System.out.println(Math.Max);
     }
 }
